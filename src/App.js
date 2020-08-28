629eac2530465aa7e452b2d6aa1eb716e95f9f95
MiMNets <mdmonirulislammonir@gmail.com>
2020-08-29 00:49:20 +0600

    added header componenet

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import News from './components/News/News';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div>
       <h2>React Bootstrap</h2>
+      <Header></Header>
       <News></News>
     </div>
   );
